8ed23e2ac73cdc6955e9b834ff45ae7941cf3da0
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-06-25 12:32:09 -0500

    added map & geo coding

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,2 +1,26 @@
 "use strict";
 
+function initMap() {
+    var mapCanvas = document.getElementById("map-canvas");
+    var mapOptions = {
+        zoom: 5,
+        center: {
+            lat: 29.426791,
+            lng: -98.48602
+        }
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+    var geo = new google.maps.Geocoder();
+
+    geo.geocode({address: "910 S Alamo St"}, function (response, status) {
+        if (status === "OK") {
+            console.log(response[0].geometry.location.lat());
+            console.log(response[0].geometry.location.lng());//getting lat & long
+// map.setCenter(response[0].geometry.location);
+        } else {
+            alert("invalid address!");
+        }
+    });
+}
