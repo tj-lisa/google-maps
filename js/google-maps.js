22035e682ab67e64d0f612eb2ebc770245b9fcaa
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-06-25 15:11:03 -0500

    Put restaurants into array

--- js/google-maps.js
+++ js/google-maps.js
@@ -14,7 +14,7 @@ function initMap() {
 
     var geo = new google.maps.Geocoder();
 
-    geo.geocode({address: "424 E Nueva St, San Antonio"}, function (response, status) {
+    geo.geocode({address: "101 Alamo Plaza, San Antonio"}, function (response, status) {
         if (status === "OK") {
             console.log(response[0].geometry.location.lat());
             console.log(response[0].geometry.location.lng());
@@ -24,49 +24,70 @@ function initMap() {
         }
     });
 
-    var rosario = new google.maps.Marker({
-        position: {
-            lat: 29.4154031,
-            lng: -98.4895912
+
+    var rosarios = {
+        marker: {
+            position: {
+                lat: 29.4154031,
+                lng: -98.4895912
+            },
+            animation: google.maps.Animation.DROP,
+            map: map
         },
-        map: map
-    });
-    var rita = new google.maps.Marker({
-        position: {
-            lat: 29.424703,
-            lng: -98.48999029999999
+        infoWindow: {
+            content: "<h1>Rosario's</h1>\n" +
+            "<h3>Great Margaritas!</h3>\n" +
+            "<h3>Live Music!</h3>\n" +
+            "<h3>Open Late</h3>"
+        }
+    };
+
+    var ritas = {
+        marker: {
+            position: {
+                lat: 29.424703,
+                lng: -98.48999029999999
+            },
+            animation: google.maps.Animation.DROP,
+            map: map
         },
-        map: map
-    });
+        infoWindow: {
+            content: "<h1>Rita's</h1>\n" +
+            "<h3>Great Margaritas!</h3>\n" +
+            "<h3>Live Music!</h3>\n" +
+            "<h3>Open Late</h3>"
+        }
+    };
+
+    var mcdonalds = {
+        marker: {
+            position: {
+                lat: 29.423843,
+                lng: -98.48746210000002
+            },
+            animation: google.maps.Animation.DROP,
+            map: map
+        },
+        infoWindow: {
+            content: "<h1>McDonalds's</h1>\n" +
+            "<h3>Bad service!</h3>\n" +
+            "<h3>Good fries!</h3>\n" +
+            "<h3>Open always.</h3>"
+        }
+    };
 
 
+    var restaurants = [
+        rosarios, ritas, mcdonalds
+    ];
 
-    var rosarioInfo = new google.maps.InfoWindow({
-        content: "<h1>Rosario's</h1>\n" +
-        "<h3>Great Margaritas!</h3>\n" +
-        "<h3>Live Music!</h3>\n" +
-        "<h3>Open Late</h3>"
-
-
-    });
-
-    google.maps.event.addListener(rosario, "click", function(){
-        // console.log("Fire");
-        rosarioInfo.open(map, rosario);
+    restaurants.forEach( function (restaurant) {
+        var tempMarker = new google.maps.Marker(restaurant.marker);
+        var infoBox = new google.maps.InfoWindow(restaurant.infoWindow);
+        google.maps.event.addListener(tempMarker, "click", function(){
+            console.log('hello');
+            infoBox.open(map, tempMarker);
+        })
     })
 
-    // infoWindow.open(map, rosarios);
-
-    var ritaInfo = new google.maps.InfoWindow({
-        content: "<h1>Rita's</h1>\n" +
-        "<h3>Great Margaritas!</h3>\n" +
-        "<h3>Live Music!</h3>\n" +
-        "<h3>Open Late</h3>"
-    });
-
-    google.maps.event.addListener(rita, "click", function(){
-        // console.log("Fire");
-        ritaInfo.open(map, rita);
-    })
-    // infoWindow.open(map, ritas);
 }
